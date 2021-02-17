255e6baff417319a0723219d0d7fdd948d39734b
Bhuvi Gupta <bhuveh@users.noreply.github.com>
2021-02-17 16:47:12 +0530

    JS Stats fix, some CSS fixes, favicon.

--- js/odometerTrigger.js
+++ js/odometerTrigger.js
@@ -7,6 +7,26 @@ const youtubeUserGeetsly = "UC8gT5G2ktLGoHlLm33EwOsA"; // Geetsly
 let subCount = 0;
 let viewCount = 0;
 
+/** Odometer Trigger function */
+function odometerTrigger() {
+  /** If you want to change any of the statistics number, these are the numbers you want to change below: */
+
+  /** Subscriber Count */
+  setTimeout(function () {
+    subOdometer.innerHTML = subCount ? subCount : 535090;
+  }, 0);
+
+  /** View Count */
+  setTimeout(function () {
+    viewOdometer.innerHTML = viewCount ? viewCount : 144531019;
+  }, 0);
+
+  /** Channel Count */
+  setTimeout(function () {
+    channelOdometer.innerHTML = 4;
+  }, 0);
+}
+
 let getSubscribers = () => {
   fetch(
     `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUserFront}&key=${youtubeKey}`
@@ -17,75 +37,52 @@ let getSubscribers = () => {
     .then((data) => {
       subCount += parseInt(data["items"][0].statistics.subscriberCount);
       viewCount += parseInt(data["items"][0].statistics.viewCount);
-    });
-
-  fetch(
-    `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUserJack}&key=${youtubeKey}`
-  )
-    .then((response) => {
-      return response.json();
+      /* console.log(subCount + ' ' + viewCount); */
     })
-    .then((data) => {
-      subCount += parseInt(data["items"][0].statistics.subscriberCount);
-      viewCount += parseInt(data["items"][0].statistics.viewCount);
-    });
-
-  fetch(
-    `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUserBraved}&key=${youtubeKey}`
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      subCount += parseInt(data["items"][0].statistics.subscriberCount);
-      viewCount += parseInt(data["items"][0].statistics.viewCount);
-    });
-
-  fetch(
-    `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUserGeetsly}&key=${youtubeKey}`
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      subCount += parseInt(data["items"][0].statistics.subscriberCount);
-      viewCount += parseInt(data["items"][0].statistics.viewCount);
-    });
+    .then (() => {
+      fetch(
+        `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUserJack}&key=${youtubeKey}`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          subCount += parseInt(data["items"][0].statistics.subscriberCount);
+          viewCount += parseInt(data["items"][0].statistics.viewCount);
+          /* console.log(subCount + ' ' + viewCount); */
+        })
+        .then (() => {
+          fetch(
+            `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUserBraved}&key=${youtubeKey}`
+          )
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+              subCount += parseInt(data["items"][0].statistics.subscriberCount);
+              viewCount += parseInt(data["items"][0].statistics.viewCount);
+              /* console.log(subCount + ' ' + viewCount); */
+            })
+            .then (() => {
+              fetch(
+                `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUserGeetsly}&key=${youtubeKey}`
+              )
+                .then((response) => {
+                  return response.json();
+                })
+                .then((data) => {
+                  subCount += parseInt(data["items"][0].statistics.subscriberCount);
+                  viewCount += parseInt(data["items"][0].statistics.viewCount);
+                  /* console.log(subCount + ' ' + viewCount); */
+                })
+                .then(() => {
+                  odometerTrigger();
+                });
+            });
+        });
+    });    
 };
+
 
 getSubscribers();
 
-/** Odometer Trigger function */
-function odometerTrigger() {
-  /** If you want to change any of the statistics number, these are the numbers you want to change below: */
-
-  /** Subscriber Count */
-  setTimeout(function () {
-    subOdometer.innerHTML = subCount ? subCount : 535090;
-  });
-
-  /** View Count */
-  setTimeout(function () {
-    viewOdometer.innerHTML = viewCount ? viewCount : 144531019;
-  });
-
-  /** Channel Count */
-  setTimeout(function () {
-    channelOdometer.innerHTML = 4;
-  });
-}
-
-odometerTrigger();
-
-/** You don't need to mess with this */
-/** Removed cuz section is now at the top, needs to trigger without scroll! */
-/** Calculate the distance to the odometer in order to trigger on scroll
-$(window).scroll(function () {
-  var odometerTop = $("#subOdometer").offset().top,
-    odometerHeight = $("#subOdometer").outerHeight(),
-    windowHeight = $(window).height(),
-    windowScroll = $(this).scrollTop();
-  if (windowScroll > odometerTop + odometerHeight - windowHeight) {
-    odometerTrigger();
-  }
-});*/
